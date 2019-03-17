566af3c8e5cc414339ac5a07eec6977187099367
plokmij <samfan@live.com>
2019-03-17 15:53:04 +0530

    define colors

---
+++ lib/src/screens/home.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget{
+  Widget build(BuildContext coontext){
+    return Scaffold(
+      body: Text("Hello Turf"),
+    );
+  }
+}
---
+++ lib/src/styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class TurfColors {
+  static Color red = Color(0xffff4c5d);
+  static Color bgColor = Color(0xcad8e4);
+  static Color blue =Color(0xff214489);
+  static Color black =  Color(0xff0a1525);
+  static Color white = Colors.white;
+}
