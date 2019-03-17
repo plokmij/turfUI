e5e8553b39a7fc30de283c5a4cba47b563671d71
plokmij <samfan@live.com>
2019-03-17 16:02:39 +0530

    bootom navbar

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'screens/home.dart';
+import 'styles.dart';
 
 class App extends StatelessWidget {
   Widget build(BuildContext context){
     return MaterialApp(
       home: Home(),
+      theme: ThemeData(
+        primaryColor: TurfColors.red, 
+      ),
     );
   }
 }
--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,9 +1,43 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget{
-  Widget build(BuildContext coontext){
+class Home extends StatelessWidget {
+  Widget build(BuildContext coontext) {
     return Scaffold(
-      body: Text("Hello Turf"),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: 0,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+            ),
+            title: Text("Home"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+            ),
+            title: Text("Search"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.crop_free,
+            ),
+            title: Text('Scan'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.remove_red_eye,
+            ),
+            title: Text(
+              'Prediction',
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text("Hello Turf"),
+      ),
     );
   }
 }
